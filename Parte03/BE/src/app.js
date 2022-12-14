20702df0ec88b75f4222852772d1940eb9df37b6
luis1310 <43614832+luis1310@users.noreply.github.com>
2022-12-14 10:35:46 -0500

    inicio script backend

---
+++ Parte03/BE/src/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+const control_cliente = require('./controllers/control_cliente')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+  control_cliente.getclients()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/merchants', (req, res) => {
+  control_cliente.createclient(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/merchants/:id', (req, res) => {
+  control_cliente.deleteclient(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ Parte03/BE/src/controllers/control_cliente.js
@@ -0,0 +1,48 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'inkaTravel',
+  password: 'luis1310',
+  port: 5432,
+});
+
+const getclients = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM clients ORDER BY idclient ASC', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+  const createclient = (body) => {
+    return new Promise(function(resolve, reject) {
+      const { name, email, password } = body
+      pool.query('INSERT INTO clients (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, password], (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(`A new client has been added added: ${results.rows[0]}`)
+      })
+    })
+  }
+  const deleteclient = () => {
+    return new Promise(function(resolve, reject) {
+      const idclient = parseInt(request.params.idclient)
+      pool.query('DELETE FROM clients WHERE id = $1', [idclient], (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(`client deleted with ID: ${idclient}`)
+      })
+    })
+  }
+  
+  module.exports = {
+    getclients,
+    createclient,
+    deleteclient,
+  }
+  
